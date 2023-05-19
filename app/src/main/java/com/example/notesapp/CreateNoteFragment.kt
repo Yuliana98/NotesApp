7136a5c45f13dc7e3bdcdb93ed62f9ba41bf43e8
Julijana <yulianane@yandex.ru>
2023-05-19 16:20:07 +0200

    CreateNoteFragment

--- app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
@@ -1,10 +1,14 @@
 package com.example.notesapp
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.Date
 
 class CreateNoteFragment : Fragment() {
 
@@ -30,5 +34,38 @@ class CreateNoteFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val simpleDateFormat = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = simpleDateFormat.format(Date())
+        val tvDateTime = view.findViewById<TextView>(R.id.tvDateTime)
+        val imgDone = view.findViewById<ImageView>(R.id.imgDone)
+        val imgBack = view.findViewById<ImageView>(R.id.imgBack)
+
+        tvDateTime.text = currentDate
+
+        imgDone.setOnClickListener {
+            saveNote()
+        }
+
+        imgBack.setOnClickListener {
+            replaceFragment(HomeFragment.newInstance(), false)
+        }
+    }
+
+    private fun saveNote() {
+//        TODO: save Note
+    }
+
+    fun replaceFragment(fragment: Fragment, isTransition: Boolean) {
+        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
+
+        if (isTransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.add(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 }
